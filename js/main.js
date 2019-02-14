c561fab481b3409cbfa336af15ad329ac1d437e5
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-14 17:05:48 -0600

    Fix pdfjs error by calling for script BEFORE main.js.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("hello");
+//VARIABLES
+const url = '../docs/sample.pdf'
+
+let pdfDoc = null, // represents grabbed document
+	pageNum = 1,
+	pageIsRendering = false,
+	pageNumIsPending = null;
+
+const scale = 1.5,
+	canvas = document.querySelector('#pdf-render'),
+	ctx = canvas.getContext('2d');
+
+//FUNCTIONS
+const renderPage = num => {
+
+};
+
+//GET document
+//pdfjsLib obj provided by pdf.js
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+	pdfDoc = pdfDoc_;
+	console.log(pdfDoc);
+});
