881bd154ef30bef7881be7127aa26ea7fb848d1a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-14 17:21:24 -0600

    Render first page in canvas.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //VARIABLES
 const url = '../docs/sample.pdf'
+const pageCount = document.querySelector('#page-count');
+const pageNumber = document.querySelector('#page-num');
 
 let pdfDoc = null, // represents grabbed document
 	pageNum = 1,
@@ -12,12 +14,38 @@ const scale = 1.5,
 
 //FUNCTIONS
 const renderPage = num => {
+	pageIsRendering = true;
 
+	//getPage is a method from pdf-js, available to us because pdfDoc is, by the point, no longer null and is instead an object representing a PDF.
+	pdfDoc.getPage(num).then(page => {
+
+		//set the scale of the document from pdf-js
+		const viewport = page.getViewport({scale});
+		canvas.height = viewport.height;
+		canvas.width = viewport.width;
+
+		const renderCtx = {
+			canvasContext: ctx, viewport
+		}
+		page.render(renderCtx).promise.then(() => {
+			pageIsRendering = false;
+
+			if(pageNumIsPending) {
+				renderPage(pageNumIsPending) //includes the number of page to render
+				pageNumIsPending = null;
+			}
+		});
+
+		//output current page in the div
+		pageNumber.textContent = num;
+	});
 };
 
 //GET document
 //pdfjsLib obj provided by pdf.js
 pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
 	pdfDoc = pdfDoc_;
-	console.log(pdfDoc);
+	pageCount.textContent = pdfDoc.numPages;
+
+	renderPage(pageNum)
 });
